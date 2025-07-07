3b25bef7079a6038bb3e472930eb7ae27d3e786d
Emanuele Stoppa <my.burning@gmail.com>
2025-07-07 09:45:45 +0200

    more stuff

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
   type: 'content',
@@ -10,6 +11,19 @@ const blogCollection = defineCollection({
   }),
 });
 
+const projectsCollection = defineCollection({
+  loader: glob({ pattern: '*.json', base: './src/content/projects' }),
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    githubUrl: z.string().url(),
+    iconUrl: z.string().url(),
+    contributions: z.string().optional(),
+    order: z.number().optional(),
+  }),
+});
+
 export const collections = {
   blog: blogCollection,
+  projects: projectsCollection,
 };
