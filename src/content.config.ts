f1fb579ab41e44c6abfefd34ae578257af8cb083
Emanuele Stoppa <my.burning@gmail.com>
2025-10-17 11:53:51 +0100

    chore: use astro image

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,12 +3,12 @@ import { file, glob } from "astro/loaders";
 
 const blog = defineCollection({
 	loader: glob({ pattern: "*.{md,mdx}", base: "src/content/blog" }),
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		slug: z.string(),
 		pubDate: z.coerce.date(),
-		heroImage: z.string(),
+		heroImage: image(),
 		heroAltImage: z.string(),
 		draft: z.boolean().optional(),
 	}),
